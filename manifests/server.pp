a4a90f1a121df0781691287486fba0bd2af74dc5
Zach Leslie <zach@puppetlabs.com>
2012-12-27 13:16:48 -0800

    move os implementation details into its own class for nginx::server::debian

--- manifests/server.pp
+++ manifests/server.pp
@@ -23,35 +23,14 @@ class nginx::server (
   # We assume for our modules, we have the motd module, & use it.
   motd::register{ 'nginx': }
 
+  # Platform specific server setup items
+  case $operatingsystem {
+    'debian': { include nginx::server::debian }
+  }
+
   package{ 'nginx':
     ensure => present,
     name   => $nginx::params::package,
-  }
-
-  if $operatingsystem == "FreeBSD" {
-    Package { provider => pkgng }
-  }
-
-  if $operatingsystem == 'Debian' {
-    apt::pin{ 'nginx':
-      release  => 'squeeze-backports',
-      priority => '1001',
-      before   => Package['nginx'],
-    }
-
-    # Pull in package xz if we haven't already.
-    # See dist/packages/manifests/archive.pp
-    Package <| alias == 'xz' |>
-
-    # Debian uses logrotate, other things use other things.
-    file{ '/etc/logrotate.d/nginx':
-      ensure  => file,
-      content => template( 'nginx/debian_logrotate.erb' ),
-      mode    => '0644',
-      owner   => 'root',
-      group   => 'root',
-      require => Package['xz'],
-    }
   }
 
   service{ 'nginx':
@@ -63,7 +42,6 @@ class nginx::server (
     restart    => $nginx::params::restart,
     subscribe  => Package['nginx'],
   }
-
 
   # All the files, stolen from debian, hence this being debian
   # specific at the minute.
